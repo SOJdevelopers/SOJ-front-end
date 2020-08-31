c107f283eb8046a07ce66c0af83763724481e16a
daklqw <daklqw@outlook.com>
2020-08-31 10:10:27 +0800

    faq

--- app/controllers/faq.php
+++ app/controllers/faq.php
@@ -26,6 +26,7 @@
 		<p>Pascal 的编译器是 fpc 2.6.2，编译命令：<code>fpc code.pas -O2</code>。</p>
 		<p>Java8 的 JDK 版本是 jdk-8u152，编译命令：<code>javac code.java</code>。</p>
 		<p>Python 会先编译为优化过的字节码 <samp>.pyo</samp> 文件。支持的 Python 版本分别为 Python 2.7 和 3.4。</p>
+		<p>评测机效率见 <a href='/blog/502'> 理性理解 Stupid OJ 测评机速度</a>。</p>
 	</section>
 	<section>
 		<header>
@@ -41,7 +42,24 @@
 	</section>
 	<section>
 		<header>
-			<h4>6．交互式类型的题怎么本地测试</h4>
+			<h4>6．评论表情使用指南</h4>
+		</header>
+		<p>见 <a href="/blog/791"><?= HTML::url('/blog/791') ?></a>。</p>
+	</section>
+	<section>
+		<header>
+			<h4>7．为什么我看不了题？组是什么？</h4>
+		</header>
+		<p>组是 <?= UOJConfig::$data['profile']['oj-name-short'] ?> 的一个特色，如有雷同，那就雷同吧。</p>
+		<p>SOJ 的权限管理依赖于组，点击<a href="/groups">查看自己所在的组</a></p>
+		<p>其中标绿表示你是成员，标蓝表示你是管理员，不标表示不在组内。</p>
+		<p>用户组是参加组队比赛用的组，<?= UOJConfig::$data['profile']['oj-name-short'] ?> 的 ACM 赛制基于组功能。</p>
+		<p>系统组决定用户的权限，例如登录，看题，参加比赛，管理 OJ 等。</p>
+		<p>如果你<strong style="color: red">看不了题</strong>那是因为你被分到了未分类组，请联系管理员来获得权限。</p>
+	</section>
+	<section>
+		<header>
+			<h4>8．交互式类型的题怎么本地测试</h4>
 		</header>
 		<p>唔……好问题。交互式的题一般给了一个头文件要你 include 进来，以及一个实现接口的源文件 grader。好像大家对多个源文件一起编译还不太熟悉。</p>
 		<p>对于 C++：<code>g++ -o code grader.cpp code.cpp</code></p>
@@ -53,7 +71,7 @@
 	</section>
 	<section>
 		<header>
-			<h4>7．IO 交互式的题怎么本地测试</h4>
+			<h4>9．IO 交互式的题怎么本地测试</h4>
 		</header>
 		<p>IO 交互式的题指的是这种题型：有两个程序 <code>a</code> 和 <code>b</code>，其中 <code>a</code> 的输出连接 <code>b</code> 的输入，<code>b</code> 的输出连接 <code>a</code> 的输入。且交互过程中需要实时刷新缓存 (flush)。</p>
 		<p>这种交互题的一大优点是不受语言的限制，<del>当然你可以学习早年 IOI 使用人类智慧手动输入也是可以滴</del>。</p>
@@ -64,7 +82,7 @@
 	</section>
 	<section>
 		<header>
-			<h4>8．联系方式</h4>
+			<h4>10．联系方式</h4>
 		</header>
 		<p>如果你<strong style="color: red">想注册</strong>、想出题、想办比赛、发现了 BUG 或者对网站有什么建议，可以通过下面的方式联系我们：</p>
 		<ul>
@@ -72,7 +90,6 @@
 			<li>直接在机房里喊一声。</li>			
 			<li>你也可以进 QQ 群水水，群号是 <?= UOJConfig::$data['profile']['qq-group'] ?>，Stupid OJ <em>珂学</em>群。</li>
 		</ul>
-		<p>也许你突然发现你<strong style="color: red">看不了题</strong>那是因为你被分到了未分类组，请联系管理员来获得权限。</p>
 	</section>
 </article>
 
