7e4aeb416fd1c92325511398a34620cc63ce346f
daklqw <daklqw@outlook.com>
2020-09-08 09:12:51 +0800

    ttt

--- utility/group_scripts/operator_default.php
+++
@@ -1,3 +0,0 @@
-<?php
-$result = 'Wow! Rating:' . $args;
-?>
--- utility/group_scripts/selector_default.php
+++
@@ -1,5 +0,0 @@
-<?php
-if (!$args || !isset($args))
-	$args = 'root';
-$result = DB::selectFirst("select rating from user_info where username='{$args}'")["rating"];
-?>
