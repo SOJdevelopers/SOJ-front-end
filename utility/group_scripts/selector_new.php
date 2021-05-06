5c3f2dc6c985d1173b84f4c10ad69bb7b138b116
daklqw <daklqw@outlook.com>
2021-05-06 09:08:28 +0800

    git ignore refresh

--- utility/group_scripts/selector_new.php
+++
@@ -1,3 +0,0 @@
-<?php
-$ret = DB::selectAll('select username from user_info where not exists (select 1 from group_members where user_info.username = group_members.username)');
-$result = json_encode($ret);
