96fcc7f8274f58083df239123447ce95e583b37b
wangzhifang001 <49518077+wangzhifang001@users.noreply.github.com>
2024-07-10 21:37:11 +0800

    Update problem_set.php
    Fix a missing.

--- app/controllers/problem_set.php
+++ app/controllers/problem_set.php
@@ -10,11 +10,14 @@
 	if (isProblemCreator(Auth::user())) {
 		$new_problem_form = new UOJForm('new_problem');
 		$new_problem_form->handle = function() {
-			insertAuditLog('problems','create',$problem['id'],'','');
 			DB::insert("insert into problems (title, is_hidden, submission_requirement) values ('New Problem', 1, '{}')");
 			$id = DB::insert_id();
+			insertAuditLog('problems','create',$id,'','');
 			DB::insert("insert into problems_contents (id, statement, statement_md) values ({$id}, '', '')");
 			DB::insert("insert into problems_visibility (problem_id, group_name) values ({$id}, '" . UOJConfig::$data['profile']['common-group'] . "')");
+			if (!isProblemManager(Auth::user())) {
+				DB::insert("insert into problems_permissions (problem_id, username) values ({$id}, '" . Auth::id() . "')");
+			}
 			dataNewProblem($id);
 		};
 		$new_problem_form->submit_button_config['align'] = 'right';
