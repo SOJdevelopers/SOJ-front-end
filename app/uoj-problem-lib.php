7473cd1de066f57e76542ed0c6ad695be3b41bca
wangzhifang001 <49518077+wangzhifang001@users.noreply.github.com>
2024-07-11 14:26:10 +0800

    Create uoj-problem-lib.php

---
+++ app/uoj-problem-lib.php
@@ -0,0 +1,22 @@
+<?php
+	requirePHPLib('data');
+
+	function addProblemPermission($problem_id, $username, $log_config = array()) {
+		insertAuditLog('problems','add permission',$problem_id,isset($log_config['reason'])?$log_config['reason']:'', json_encode(array('username' => $username)), $log_config);
+		DB::insert("insert into problems_permissions (problem_id, username) values ({$problem_id}, '$username')");
+	}
+	function deleteProblemPermission($problem_id, $username, $log_config = array()) {
+		insertAuditLog('problems','delete permission',$problem_id,isset($log_config['reason'])?$log_config['reason']:'', json_encode(array('username' => $username)), $log_config);
+		DB::delete("delete from problems_permissions where problem_id = {$problem_id} and username = '$username'");
+	}
+	function newProblem($log_config = array()) {
+		DB::insert("insert into problems (title, is_hidden, submission_requirement) values ('New Problem', 1, '{}')");
+		$id = DB::insert_id();
+		insertAuditLog('problems','create',$id,isset($log_config['reason'])?$log_config['reason']:'', '', $log_config);
+		DB::insert("insert into problems_contents (id, statement, statement_md) values ({$id}, '', '')");
+		DB::insert("insert into problems_visibility (problem_id, group_name) values ({$id}, '" . UOJConfig::$data['profile']['common-group'] . "')");
+		if (!isProblemManager(Auth::user()))
+			addProblemPermission($id, Auth::id(), array('reason' => 'create problem without manage permission', 'auto' => true));
+		dataNewProblem($id);
+		return $id;
+	}
