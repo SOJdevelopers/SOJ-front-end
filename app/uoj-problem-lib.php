7fdceb3684b33ac4cbbda5a27b84c448c52ea026
wangzhifang001 <49518077+wangzhifang001@users.noreply.github.com>
2024-07-11 15:24:50 +0800

    Update uoj-problem-lib.php
    Add addProblemViewPermission, deleteProblemViewPermission.

--- app/uoj-problem-lib.php
+++ app/uoj-problem-lib.php
@@ -5,10 +5,22 @@
 		insertAuditLog('problems','add permission',$problem_id,isset($log_config['reason'])?$log_config['reason']:'', json_encode(array('username' => $username)), $log_config);
 		DB::insert("insert into problems_permissions (problem_id, username) values ({$problem_id}, '$username')");
 	}
+
 	function deleteProblemPermission($problem_id, $username, $log_config = array()) {
 		insertAuditLog('problems','delete permission',$problem_id,isset($log_config['reason'])?$log_config['reason']:'', json_encode(array('username' => $username)), $log_config);
 		DB::delete("delete from problems_permissions where problem_id = {$problem_id} and username = '$username'");
 	}
+
+	function addProblemViewPermission($problem_id, $groupname, $log_config = array()) {
+		insertAuditLog('problems','add view permission',$problem_id,isset($log_config['reason'])?$log_config['reason']:'', json_encode(array('groupname' => $groupname)), $log_config);
+		DB::insert("insert into problems_visibility (problem_id, group_name) values ({$problem_id}, '$groupname')");
+	}
+
+	function deleteProblemViewPermission($problem_id, $groupname, $log_config = array()) {
+		insertAuditLog('problems','delete view permission',$problem_id,isset($log_config['reason'])?$log_config['reason']:'', json_encode(array('groupname' => $groupname)), $log_config);
+		DB::delete("delete from problems_visibility where problem_id = {$problem_id} and group_name = '$groupname'");
+	}
+
 	function newProblem($log_config = array()) {
 		DB::insert("insert into problems (title, is_hidden, submission_requirement) values ('New Problem', 1, '{}')");
 		$id = DB::insert_id();
